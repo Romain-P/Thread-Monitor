15b4d539c69e202c811d3763860c4c6d9fe1e6f4
Romain <romain.pillot@epitech.eu>
2019-11-07 03:16:27 +0000

    indent

--- src/fr/romain/assignment3/gui/Monitor.java
+++ src/fr/romain/assignment3/gui/Monitor.java
@@ -17,157 +17,157 @@ import static fr.romain.assignment3.shared.ActorState.WAITING;
 import static fr.romain.assignment3.shared.ActorType.STUDENT;
 
 public class Monitor extends JPanel {
-	private final RoomMonitorPanel w101;
-	private final RoomMonitorPanel w102;
-	private final RoomMonitorPanel w201;
-	private final RoomMonitorPanel w202;
-	private final HashMap<ClassRoomId, BlockingQueue<Runnable>> tasks;
-	private final Timer worker;
-	private final int frameRate;
+    private final RoomMonitorPanel w101;
+    private final RoomMonitorPanel w102;
+    private final RoomMonitorPanel w201;
+    private final RoomMonitorPanel w202;
+    private final HashMap<ClassRoomId, BlockingQueue<Runnable>> tasks;
+    private final Timer worker;
+    private final int frameRate;
 
-	public Monitor(int frameRate) {
-		this.frameRate = frameRate;
-		this.tasks = new HashMap<>();
-		for (ClassRoomId id: ClassRoomId.values())
-			tasks.put(id, new LinkedBlockingQueue<>());
-		this.worker = pollEvents();
+    public Monitor(int frameRate) {
+        this.frameRate = frameRate;
+        this.tasks = new HashMap<>();
+        for (ClassRoomId id : ClassRoomId.values())
+            tasks.put(id, new LinkedBlockingQueue<>());
+        this.worker = pollEvents();
 
-		setLayout(null);
-		w101 = new RoomMonitorPanel("W101", 10, 11, 406, 371, 20);
-		add(w101);
-		w201 = new RoomMonitorPanel("W201", 523, 11, 406, 371, 60);
-		add(w201);
-		w102 = new RoomMonitorPanel("W102", 10, 403, 406, 371, 30);
-		add(w102);
-		w202 = new RoomMonitorPanel("W202", 523, 403, 406, 371, 60);
-		add(w202);
-	}
+        setLayout(null);
+        w101 = new RoomMonitorPanel("W101", 10, 11, 406, 371, 20);
+        add(w101);
+        w201 = new RoomMonitorPanel("W201", 523, 11, 406, 371, 60);
+        add(w201);
+        w102 = new RoomMonitorPanel("W102", 10, 403, 406, 371, 30);
+        add(w102);
+        w202 = new RoomMonitorPanel("W202", 523, 403, 406, 371, 60);
+        add(w202);
+    }
 
-	public void onEnter(Actor actor, int place) {
-		final ClassRoomId id = actor.getClassRoom().getId();
-		final RoomMonitorPanel monitor = panel(id);
-		final boolean hasWaited = actor.getActorState() == WAITING;
+    public void onEnter(Actor actor, int place) {
+        final ClassRoomId id = actor.getClassRoom().getId();
+        final RoomMonitorPanel monitor = panel(id);
+        final boolean hasWaited = actor.getActorState() == WAITING;
 
-		postEvent(id, () -> {
-			switch (actor.type()) {
-				case STUDENT:
-					if (hasWaited)
-						monitor.delWaitingStudent();
+        postEvent(id, () -> {
+            switch (actor.type()) {
+                case STUDENT:
+                    if (hasWaited)
+                        monitor.delWaitingStudent();
 
-					monitor.room().setColor(place, Color.ORANGE);
-					monitor.addStudent();
-					break;
-				case VISITOR:
-					if (hasWaited)
-						monitor.delWaitingVisitor();
+                    monitor.room().setColor(place, Color.ORANGE);
+                    monitor.addStudent();
+                    break;
+                case VISITOR:
+                    if (hasWaited)
+                        monitor.delWaitingVisitor();
 
-					monitor.room().setColor(place, Color.ORANGE);
-					monitor.addVisitor();
-					break;
-				case LECTURER:
-					if (hasWaited)
-						monitor.delWaitingLecturer();
+                    monitor.room().setColor(place, Color.ORANGE);
+                    monitor.addVisitor();
+                    break;
+                case LECTURER:
+                    if (hasWaited)
+                        monitor.delWaitingLecturer();
 
-					monitor.setLecturer(((Lecturer) actor).getLecturerId().name());
-					break;
-			}
-			monitor.room().repaint();
-		});
-	}
+                    monitor.setLecturer(((Lecturer) actor).getLecturerId().name());
+                    break;
+            }
+            monitor.room().repaint();
+        });
+    }
 
-	public void onLeave(Actor actor, int place) {
-		final ClassRoomId id = actor.getClassRoom().getId();
-		final RoomMonitorPanel monitor = panel(id);
+    public void onLeave(Actor actor, int place) {
+        final ClassRoomId id = actor.getClassRoom().getId();
+        final RoomMonitorPanel monitor = panel(id);
 
-		postEvent(id, () -> {
+        postEvent(id, () -> {
 
-			switch (actor.type()) {
-				case STUDENT:
-					monitor.room().setColor(place, Color.GREEN);
-					monitor.delStudent();
-					break;
-				case VISITOR:
-					monitor.room().setColor(place, Color.GREEN);
-					monitor.delVisitor();
-					break;
-				case LECTURER:
-					monitor.removeLecturer();
-					break;
-			}
-			monitor.room().repaint();
-		});
-	}
+            switch (actor.type()) {
+                case STUDENT:
+                    monitor.room().setColor(place, Color.GREEN);
+                    monitor.delStudent();
+                    break;
+                case VISITOR:
+                    monitor.room().setColor(place, Color.GREEN);
+                    monitor.delVisitor();
+                    break;
+                case LECTURER:
+                    monitor.removeLecturer();
+                    break;
+            }
+            monitor.room().repaint();
+        });
+    }
 
-	public void onSit(Actor actor, int place) {
-		final ClassRoomId id = actor.getClassRoom().getId();
-		final RoomMonitorPanel monitor = panel(id);
+    public void onSit(Actor actor, int place) {
+        final ClassRoomId id = actor.getClassRoom().getId();
+        final RoomMonitorPanel monitor = panel(id);
 
-		postEvent(id, () -> {
+        postEvent(id, () -> {
 
-			if (actor.type() == STUDENT)
-				monitor.room().setColor(place, Color.RED);
-			else
-				monitor.room().setColor(place, Color.RED);
-			monitor.room().repaint();
-		});
-	}
+            if (actor.type() == STUDENT)
+                monitor.room().setColor(place, Color.RED);
+            else
+                monitor.room().setColor(place, Color.RED);
+            monitor.room().repaint();
+        });
+    }
 
-	public void onWait(Actor actor, ClassRoom room) {
-		final ClassRoomId id = room.getId();
-		final RoomMonitorPanel monitor = panel(id);
+    public void onWait(Actor actor, ClassRoom room) {
+        final ClassRoomId id = room.getId();
+        final RoomMonitorPanel monitor = panel(id);
 
-		postEvent(id, () -> {
-			switch (actor.type()) {
-				case LECTURER:
-					monitor.addWaitingLecturer();
-					break;
-				case STUDENT:
-					monitor.addWaitingStudent();
-					break;
-				case VISITOR:
-					monitor.addWaitingVisitor();
-					break;
-			}
-		});
-	}
+        postEvent(id, () -> {
+            switch (actor.type()) {
+                case LECTURER:
+                    monitor.addWaitingLecturer();
+                    break;
+                case STUDENT:
+                    monitor.addWaitingStudent();
+                    break;
+                case VISITOR:
+                    monitor.addWaitingVisitor();
+                    break;
+            }
+        });
+    }
 
-	private RoomMonitorPanel panel(ClassRoomId id) {
-		switch (id) {
-			case W201:
-				return w201;
-			case W202:
-				return w202;
-			case W101:
-				return w101;
-			case W102:
-				return w102;
-			default:
-				return null;
-		}
-	}
+    private RoomMonitorPanel panel(ClassRoomId id) {
+        switch (id) {
+            case W201:
+                return w201;
+            case W202:
+                return w202;
+            case W101:
+                return w101;
+            case W102:
+                return w102;
+            default:
+                return null;
+        }
+    }
 
-	private Timer pollEvents() {
-		Timer worker = new Timer(frameRate, (event) -> {
-			for (BlockingQueue<Runnable> tasks: this.tasks.values()) {
-				Runnable task = tasks.poll();
+    private Timer pollEvents() {
+        Timer worker = new Timer(frameRate, (event) -> {
+            for (BlockingQueue<Runnable> tasks : this.tasks.values()) {
+                Runnable task = tasks.poll();
 
-				if (task == null) continue;
-				task.run();
-			}
-		});
+                if (task == null) continue;
+                task.run();
+            }
+        });
 
-		worker.setRepeats(true);
-		return worker;
-	}
+        worker.setRepeats(true);
+        return worker;
+    }
 
-	private void postEvent(ClassRoomId id, Runnable task) {
-		try {
-			this.tasks.get(id).put(task);
-		} catch (Exception e) {
-		}
-	}
+    private void postEvent(ClassRoomId id, Runnable task) {
+        try {
+            this.tasks.get(id).put(task);
+        } catch (Exception e) {
+        }
+    }
 
-	public Timer worker() {
-		return this.worker;
-	}
+    public Timer worker() {
+        return this.worker;
+    }
 }
--- src/fr/romain/assignment3/gui/components/RoomMonitorPanel.java
+++ src/fr/romain/assignment3/gui/components/RoomMonitorPanel.java
@@ -6,169 +6,169 @@ import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
 public class RoomMonitorPanel extends JPanel {
-	private final JTextField waitingVisitors;
-	private final JTextField waitingStudents;
-	private final JTextField waitingLecturers;
-	private final JTextField visitors;
-	private final JTextField students;
-	private final JTextField lecturer;
-	private final RoomPanel room;
-	
-	public RoomMonitorPanel(String title, int x, int y, int width, int height, int capacity) {
-		setBorder(new TitledBorder(null, title, TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		setBounds(x, y, width, height);
-		setLayout(null);
-		
-		JPanel waitingPanel = new JPanel();
-		waitingPanel.setBorder(new TitledBorder(null, "Waiting", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		waitingPanel.setBounds(10, 27, 386, 67);
-		add(waitingPanel);
-		waitingPanel.setLayout(null);
-		
-		JLabel lblVisitors = new JLabel("Visitors");
-		lblVisitors.setBounds(10, 21, 46, 14);
-		waitingPanel.add(lblVisitors);
-		
-		waitingVisitors = new JTextField("0");
-		waitingVisitors.setEditable(false);
-		waitingVisitors.setBounds(10, 36, 61, 20);
-		waitingPanel.add(waitingVisitors);
-		waitingVisitors.setColumns(10);
-		
-		waitingStudents = new JTextField("0");
-		waitingStudents.setEditable(false);
-		waitingStudents.setColumns(10);
-		waitingStudents.setBounds(161, 36, 61, 20);
-		waitingPanel.add(waitingStudents);
-		
-		JLabel lblStudents = new JLabel("Students");
-		lblStudents.setBounds(161, 21, 46, 14);
-		waitingPanel.add(lblStudents);
-		
-		waitingLecturers = new JTextField("0");
-		waitingLecturers.setEditable(false);
-		waitingLecturers.setColumns(10);
-		waitingLecturers.setBounds(315, 36, 61, 20);
-		waitingPanel.add(waitingLecturers);
-		
-		JLabel lblLecturer = new JLabel("Lecturers");
-		lblLecturer.setBounds(315, 21, 46, 14);
-		waitingPanel.add(lblLecturer);
-		
-		JPanel roomPanel = new JPanel();
-		roomPanel.setBorder(new TitledBorder(null, "Room", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		roomPanel.setBounds(10, 105, 386, 255);
-		add(roomPanel);
-		roomPanel.setLayout(null);
-		
-		visitors = new JTextField("0");
-		visitors.setEditable(false);
-		visitors.setColumns(10);
-		visitors.setBounds(10, 52, 61, 20);
-		roomPanel.add(visitors);
-		
-		JLabel label = new JLabel("Visitors");
-		label.setBounds(10, 37, 46, 14);
-		roomPanel.add(label);
-		
-		students = new JTextField("0");
-		students.setEditable(false);
-		students.setColumns(10);
-		students.setBounds(10, 129, 61, 20);
-		roomPanel.add(students);
-		
-		JLabel label_1 = new JLabel("Students");
-		label_1.setBounds(10, 114, 46, 14);
-		roomPanel.add(label_1);
-		
-		lecturer = new JTextField();
-		lecturer.setEditable(false);
-		lecturer.setColumns(10);
-		lecturer.setBounds(10, 209, 61, 20);
-		roomPanel.add(lecturer);
-		
-		JLabel label_2 = new JLabel("Lecturer");
-		label_2.setBounds(10, 194, 46, 14);
-		roomPanel.add(label_2);
-		
-		room = new RoomPanel(222, 222, capacity);
-		room.setBounds(123, 22, 222, 222);
-		roomPanel.add(room);
-	}
+    private final JTextField waitingVisitors;
+    private final JTextField waitingStudents;
+    private final JTextField waitingLecturers;
+    private final JTextField visitors;
+    private final JTextField students;
+    private final JTextField lecturer;
+    private final RoomPanel room;
 
-	public RoomPanel room() {
-		return this.room;
-	}
+    public RoomMonitorPanel(String title, int x, int y, int width, int height, int capacity) {
+        setBorder(new TitledBorder(null, title, TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        setBounds(x, y, width, height);
+        setLayout(null);
 
-	public void addWaitingLecturer() {
-		int count = Integer.parseInt(waitingLecturers.getText());
-		String sCount = String.valueOf(++count);
-		waitingLecturers.setText(sCount);
-	}
+        JPanel waitingPanel = new JPanel();
+        waitingPanel.setBorder(new TitledBorder(null, "Waiting", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        waitingPanel.setBounds(10, 27, 386, 67);
+        add(waitingPanel);
+        waitingPanel.setLayout(null);
 
-	public void delWaitingLecturer() {
-		int count = Integer.parseInt(waitingLecturers.getText());
-		String sCount = String.valueOf(--count);
-		waitingLecturers.setText(sCount);
-	}
+        JLabel lblVisitors = new JLabel("Visitors");
+        lblVisitors.setBounds(10, 21, 46, 14);
+        waitingPanel.add(lblVisitors);
 
-	public void addWaitingVisitor() {
-		int count = Integer.parseInt(waitingVisitors.getText());
-		String sCount = String.valueOf(++count);
-		waitingVisitors.setText(sCount);
-	}
+        waitingVisitors = new JTextField("0");
+        waitingVisitors.setEditable(false);
+        waitingVisitors.setBounds(10, 36, 61, 20);
+        waitingPanel.add(waitingVisitors);
+        waitingVisitors.setColumns(10);
 
-	public void delWaitingVisitor() {
-		int count = Integer.parseInt(waitingVisitors.getText());
-		String sCount = String.valueOf(--count);
-		waitingVisitors.setText(sCount);
-	}
+        waitingStudents = new JTextField("0");
+        waitingStudents.setEditable(false);
+        waitingStudents.setColumns(10);
+        waitingStudents.setBounds(161, 36, 61, 20);
+        waitingPanel.add(waitingStudents);
 
-	public void addWaitingStudent() {
-		int count = Integer.parseInt(waitingStudents.getText());
-		String sCount = String.valueOf(++count);
-		waitingStudents.setText(sCount);
-	}
+        JLabel lblStudents = new JLabel("Students");
+        lblStudents.setBounds(161, 21, 46, 14);
+        waitingPanel.add(lblStudents);
 
-	public void delWaitingStudent() {
-		int count = Integer.parseInt(waitingStudents.getText());
-		String sCount = String.valueOf(--count);
-		waitingStudents.setText(sCount);
-	}
+        waitingLecturers = new JTextField("0");
+        waitingLecturers.setEditable(false);
+        waitingLecturers.setColumns(10);
+        waitingLecturers.setBounds(315, 36, 61, 20);
+        waitingPanel.add(waitingLecturers);
 
-	public void addVisitor() {
-		int count = Integer.parseInt(visitors.getText());
-		String sCount = String.valueOf(++count);
-		visitors.setText(sCount);
-	}
+        JLabel lblLecturer = new JLabel("Lecturers");
+        lblLecturer.setBounds(315, 21, 46, 14);
+        waitingPanel.add(lblLecturer);
 
-	public void delVisitor() {
-		int count = Integer.parseInt(visitors.getText());
-		String sCount = String.valueOf(--count);
-		visitors.setText(sCount);
-	}
+        JPanel roomPanel = new JPanel();
+        roomPanel.setBorder(new TitledBorder(null, "Room", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        roomPanel.setBounds(10, 105, 386, 255);
+        add(roomPanel);
+        roomPanel.setLayout(null);
 
-	public void addStudent() {
-		int count = Integer.parseInt(students.getText());
-		String sCount = String.valueOf(++count);
-		students.setText(sCount);
-	}
+        visitors = new JTextField("0");
+        visitors.setEditable(false);
+        visitors.setColumns(10);
+        visitors.setBounds(10, 52, 61, 20);
+        roomPanel.add(visitors);
 
-	public void delStudent() {
-		int count = Integer.parseInt(students.getText());
-		String sCount = String.valueOf(--count);
-		students.setText(sCount);
-	}
+        JLabel label = new JLabel("Visitors");
+        label.setBounds(10, 37, 46, 14);
+        roomPanel.add(label);
 
-	public void removeLecturer() {
-		lecturer.setText("");
-	}
+        students = new JTextField("0");
+        students.setEditable(false);
+        students.setColumns(10);
+        students.setBounds(10, 129, 61, 20);
+        roomPanel.add(students);
 
-	public void setLecturer(String lecturer) {
-		this.lecturer.setText(lecturer);
-	}
+        JLabel label_1 = new JLabel("Students");
+        label_1.setBounds(10, 114, 46, 14);
+        roomPanel.add(label_1);
 
-	public RoomPanel getRoom() {
-		return room;
-	}
+        lecturer = new JTextField();
+        lecturer.setEditable(false);
+        lecturer.setColumns(10);
+        lecturer.setBounds(10, 209, 61, 20);
+        roomPanel.add(lecturer);
+
+        JLabel label_2 = new JLabel("Lecturer");
+        label_2.setBounds(10, 194, 46, 14);
+        roomPanel.add(label_2);
+
+        room = new RoomPanel(222, 222, capacity);
+        room.setBounds(123, 22, 222, 222);
+        roomPanel.add(room);
+    }
+
+    public RoomPanel room() {
+        return this.room;
+    }
+
+    public void addWaitingLecturer() {
+        int count = Integer.parseInt(waitingLecturers.getText());
+        String sCount = String.valueOf(++count);
+        waitingLecturers.setText(sCount);
+    }
+
+    public void delWaitingLecturer() {
+        int count = Integer.parseInt(waitingLecturers.getText());
+        String sCount = String.valueOf(--count);
+        waitingLecturers.setText(sCount);
+    }
+
+    public void addWaitingVisitor() {
+        int count = Integer.parseInt(waitingVisitors.getText());
+        String sCount = String.valueOf(++count);
+        waitingVisitors.setText(sCount);
+    }
+
+    public void delWaitingVisitor() {
+        int count = Integer.parseInt(waitingVisitors.getText());
+        String sCount = String.valueOf(--count);
+        waitingVisitors.setText(sCount);
+    }
+
+    public void addWaitingStudent() {
+        int count = Integer.parseInt(waitingStudents.getText());
+        String sCount = String.valueOf(++count);
+        waitingStudents.setText(sCount);
+    }
+
+    public void delWaitingStudent() {
+        int count = Integer.parseInt(waitingStudents.getText());
+        String sCount = String.valueOf(--count);
+        waitingStudents.setText(sCount);
+    }
+
+    public void addVisitor() {
+        int count = Integer.parseInt(visitors.getText());
+        String sCount = String.valueOf(++count);
+        visitors.setText(sCount);
+    }
+
+    public void delVisitor() {
+        int count = Integer.parseInt(visitors.getText());
+        String sCount = String.valueOf(--count);
+        visitors.setText(sCount);
+    }
+
+    public void addStudent() {
+        int count = Integer.parseInt(students.getText());
+        String sCount = String.valueOf(++count);
+        students.setText(sCount);
+    }
+
+    public void delStudent() {
+        int count = Integer.parseInt(students.getText());
+        String sCount = String.valueOf(--count);
+        students.setText(sCount);
+    }
+
+    public void removeLecturer() {
+        lecturer.setText("");
+    }
+
+    public void setLecturer(String lecturer) {
+        this.lecturer.setText(lecturer);
+    }
+
+    public RoomPanel getRoom() {
+        return room;
+    }
 }
